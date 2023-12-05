e3a47140647cffcaf755bbcea89b31fdd997d112
Joshua Hicks <joshua.brock.hicks@gmail.com>
2023-12-04 21:44:52 -0700

    day 2 part 1 solved

--- ts/day-2/part-1.ts
+++ ts/day-2/part-1.ts
@@ -1,30 +1,32 @@
 import { getFileData } from "../lib/common-functions";
-const bagContents = { red: 12, green: 13, blue: 14 };
+interface BagContents {
+    [color: string]: number;
+}
+const bagContents: BagContents = { red: 12, green: 13, blue: 14 };
 
 let data = getFileData("data");
-
-let mappedData = data.map((item) => {
-  item = item.replace(/Game \d: /g, "");
-  let moves = item.split(";").map((move) =>
-    move.split(",").map((item) => {
-      let [count, color] = item.trim().split(" ");
-      console.log({
-        [color]: parseInt(count),
-        isValid: bagContents[color] >= count,
-      });
-      return { [color]: parseInt(count), isValid: bagContents[color] >= count };
-    })
-  );
-  return moves;
-});
-
+// let data = getFileData("exampleData");
 let answer = 0;
 
-for (let game in mappedData) {
-  mappedData[game].isValid = mappedData[game].every((move) =>
-    move.every((ball) => ball["isValid"])
-  );
-  console.log(`Game ${parseInt(game) + 1}: ${mappedData[game].isValid}`);
-  if (mappedData[game].isValid) answer += parseInt(game) + 1;
+for (let line of data) {
+    // get the game #
+    const gameNumber = parseInt(line.substring(4, line.indexOf(":")));
+    line = line.substring(line.indexOf(":") + 1);
+    const ballStrings = line.split(/[,;]/).map(s => s.trim());
+    let isValid = true;
+
+    for (let ballString of ballStrings) {
+        const ballCount = parseInt(ballString.substring(0, ballString.indexOf(" ")));
+        const ballColor = ballString.substring(ballString.indexOf(" ")).trim();
+        isValid = bagContents[ballColor] >= ballCount;
+        if (!isValid) {
+            break;
+        }
+    }
+    console.log(`Game ${gameNumber} is ${isValid ? "valid" : "invalid"}`);
+    if (isValid) {
+        answer += gameNumber;
+    }
 }
+
 console.log(`Answer: ${answer}`);
