cb49fbaf57cd169a5d07b4053bca2235b9e26c94
Josh Hicks <joshua.brock.hicks@gmail.com>
2023-12-04 20:55:37 -0700

    day 2 WIP

--- ts/day-2/part-1.ts
+++ ts/day-2/part-1.ts
@@ -1,46 +1,30 @@
 import { getFileData } from "../lib/common-functions";
-
-const bagContents = { 'red': 12, 'green': 13, 'blue': 14 };
+const bagContents = { red: 12, green: 13, blue: 14 };
 
 let data = getFileData("data");
-let mappedData = data.map(item => {
-    let match = item.match(/Game (\d):/);
-    let gameNumber = match ? match[1] : null;
-    item = item.replace(/Game \d: /g, '');
-    let moves = item.split(';').map(move =>
-        move.split(',').map(item => {
-            let [count, color] = item.trim().split(' ');
-            return { [color]: parseInt(count), 'isValid': true };
-        })
-    );
-    return moves;
+
+let mappedData = data.map((item) => {
+  item = item.replace(/Game \d: /g, "");
+  let moves = item.split(";").map((move) =>
+    move.split(",").map((item) => {
+      let [count, color] = item.trim().split(" ");
+      console.log({
+        [color]: parseInt(count),
+        isValid: bagContents[color] >= count,
+      });
+      return { [color]: parseInt(count), isValid: bagContents[color] >= count };
+    })
+  );
+  return moves;
 });
 
-for (let game of mappedData) {
-    for (let move of game) {
-        for (let ball of move) {
-            if (typeof ball['red'] === 'number' && typeof bagContents['red'] === 'number') {
-                ball['isValid'] = ball['red'] <= bagContents['red'];
-            }
-            if (typeof ball['blue'] === 'number' && typeof bagContents['blue'] === 'number') {
-                ball['isValid'] = ball['blue'] <= bagContents['blue'];
-            }
-            if (typeof ball['green'] === 'number' && typeof bagContents['green'] === 'number') {
-                ball['isValid'] = ball['green'] <= bagContents['green'];
-            }
-        }
-    }
-    console.log(game);
-    console.log('----------------');
-    break;
+let answer = 0;
+
+for (let game in mappedData) {
+  mappedData[game].isValid = mappedData[game].every((move) =>
+    move.every((ball) => ball["isValid"])
+  );
+  console.log(`Game ${parseInt(game) + 1}: ${mappedData[game].isValid}`);
+  if (mappedData[game].isValid) answer += parseInt(game) + 1;
 }
-
-// filter out mappedData where any game's moves contain an invalid ball
-mappedData = mappedData.filter(game => {
-    return game.every(move => {
-        return move.every(ball => {
-            return ball['isValid'];
-        });
-    });
-});
-console.log(JSON.stringify(mappedData, null, 2));
+console.log(`Answer: ${answer}`);
