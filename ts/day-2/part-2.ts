9a43aae19a3713ced416526bd25813f65dd415c5
Josh Hicks <joshua.brock.hicks@gmail.com>
2023-12-10 10:23:24 -0700

    jplajsdf;lk

--- ts/day-2/part-2.ts
+++ ts/day-2/part-2.ts
@@ -1,28 +1,43 @@
 import { getFileData } from "../lib/common-functions";
-interface BagContents {
-    [color: string]: number;
-}
-const bagContents: BagContents = { red: 12, green: 13, blue: 14 };
 
-// let data = getFileData("data");
-let data = getFileData("exampleData");
+let data = getFileData("data");
+// let data = getFileData("exampleData");
+let powers: Array<number> = [];
 let answer = 0;
 
 for (let line of data) {
-    const gameNumber = parseInt(line.substring(4, line.indexOf(":")));
-    line = line.substring(line.indexOf(":") + 1);
-    const ballStrings = line.split(/[,;]/).map(s => s.trim());
-    let isValid = true;
-    let balls: Array<{ color: string, count: number }> = [];
+  line = line.substring(line.indexOf(":") + 1);
+  const ballStrings = line.split(/[,;]/).map((s) => s.trim());
+  let balls: Array<{ color: string; count: number }> = [];
+  line = line.substring(line.indexOf(":") + 1);
 
-    line = line.substring(line.indexOf(":") + 1);
-    for (let ballString of ballStrings) {
-        const ballCount = parseInt(ballString.substring(0, ballString.indexOf(" ")));
-        const ballColor = ballString.substring(ballString.indexOf(" ")).trim();
-        balls.push({ color: ballColor, count: ballCount });
+  for (let ballString of ballStrings) {
+    const ballCount = parseInt(
+      ballString.substring(0, ballString.indexOf(" "))
+    );
+    const ballColor = ballString.substring(ballString.indexOf(" ")).trim();
+
+    balls.push({ color: ballColor, count: ballCount });
+  }
+  // reduce the balls array to only contain one of each color, highest count wins
+  balls = balls.reduce((acc, cur) => {
+    const existingBall = acc.find((ball) => ball.color === cur.color);
+    if (existingBall) {
+      if (existingBall.count < cur.count) {
+        existingBall.count = cur.count;
+      }
+    } else {
+      acc.push(cur);
     }
-    console.log(balls);
+    return acc;
+  }, []);
+  console.log(balls);
 
+  const power = balls.reduce((acc, cur) => acc * cur.count, 1);
+  powers.push(power);
+  console.log("-----------------");
 }
-
+// add up the powers
+answer = powers.reduce((acc, cur) => acc + cur, 0);
+console.log(powers);
 console.log(`Answer: ${answer}`);
